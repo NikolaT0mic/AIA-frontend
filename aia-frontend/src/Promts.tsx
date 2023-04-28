df832d74fc24b50649b32033e4d661de396b23b1
Sven Lüpke <luepke@in.tum.de>
2023-04-29 00:49:33 +0200

    Add key

---
+++ aia-frontend/src/Promts.tsx
@@ -0,0 +1,33 @@
+const API_ENDPOINT = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+async function getChatGPTResponse(prompt: string): Promise<string> {
+  const apiKey = "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";
+  const response = await fetch(API_ENDPOINT, {
+    method: 'POST',
+    
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      'prompt': prompt,
+      'max_tokens': 64,
+      'temperature': 0.7,
+      'n': 1,
+      'stop': '\n',
+    }),
+  });
+
+  const { choices } = await response.json();
+  const { text } = choices[0];
+  return text.trim();
+}
+
+// Usage example:
+const prompt = 'Hello, ChatGPT!';
+const apiKey = 'your-openai-api-key-here';
+
+(async () => {
+  const response = await getChatGPTResponse(prompt, apiKey);
+  console.log(response);
+})();
