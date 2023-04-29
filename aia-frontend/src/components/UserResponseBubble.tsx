7505f9c355fda5e62cade6460bcfdc9a449bfd64
Nikola <niktom@web.de>
2023-04-29 15:50:12 +0200

    bubbles now working

--- aia-frontend/src/components/UserResponseBubble.tsx
+++ aia-frontend/src/components/UserResponseBubble.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 function UserResponseBubble({message}:{message:string}) {
     return (
-        <Box justifySelf="end" textAlign="end" borderWidth='2px' bgColor="gray.100" borderRadius='lg' m="5px" p="4px" minW="250px" width="fit-content">
+        <Box alignSelf="flex-end" textAlign="end" borderWidth='2px' bgColor="gray.100" borderRadius='lg' m="5px" p="4px" minW="250px" width="fit-content">
             {message}
         </Box>
     );
